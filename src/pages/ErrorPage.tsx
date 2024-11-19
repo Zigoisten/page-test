886b0f7f27101daffa8f0b48c0cf08849fea4c1c
Utónév Vezetéknév <markveseje@gmail.com>
2024-11-19 11:14:51 +0100

    routing szamologep + egysmas

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+function ErrorPage() { 
+    return (
+        <div>
+            <h1>Error: 404</h1>
+            <p>Hibás route!</p>
+        </div>
+    )
+}
+
+export default ErrorPage;
---
+++ src/pages/Obasphere.tsx
@@ -0,0 +1,14 @@
+import logo from "../logo.svg";
+import "./App.css";
+
+function Obasphere() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    )
+}
+
+export default Obasphere;
